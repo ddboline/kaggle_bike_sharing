8d72f77517cf461684151255484dd76d38dc866c
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-21 06:25:15 -0500

    ...

--- my_model.py
+++ my_model.py
@@ -39,7 +39,7 @@ def load_data():
     xtest = test_df.values
     ytest = sub_df['datetime'].values
    
-    print ytrain[:10,:]
+    print ytrain[:10]
    
     return xtrain, ytrain, xtest, ytest
 
